c179f75e4b94c1d22f712c06ddf6a34cc50b6654
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2026-06-29 14:23:12 +0530

    Refactoring

--- apps/web/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-import babel from "@rolldown/plugin-babel";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-});
